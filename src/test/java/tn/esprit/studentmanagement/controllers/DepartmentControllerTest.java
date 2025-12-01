fe7a5be5dea555a5fb85c1240aafb79d1f00edd5
sou9 <sou9@localhost>
2025-12-01 20:59:04 +0100

    fix-sonar-mockito

--- src/test/java/tn/esprit/studentmanagement/controllers/DepartmentControllerTest.java
+++ src/test/java/tn/esprit/studentmanagement/controllers/DepartmentControllerTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import tn.esprit.studentmanagement.entities.Department;
@@ -26,7 +26,7 @@ class DepartmentControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
+    @MockitoBean
     private IDepartmentService departmentService;
 
     @Autowired
--- src/test/java/tn/esprit/studentmanagement/controllers/EnrollmentControllerTest.java
+++ src/test/java/tn/esprit/studentmanagement/controllers/EnrollmentControllerTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import tn.esprit.studentmanagement.entities.Enrollment;
@@ -28,7 +28,7 @@ class EnrollmentControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
+    @MockitoBean
     private IEnrollment enrollmentService;
 
     @Autowired
